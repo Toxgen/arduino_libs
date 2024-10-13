58dd922c35790636dc021fff6d8b73fb2f433c51
Toxgen <Hydro_Toxgen@outlook.com>
2024-10-12 21:56:23 -0400

    s

--- screen.h
+++ screen.h
@@ -9,18 +9,11 @@ class first
     public:
         first(void);
         void print_out(char*);
-        void print_out_int(int)
+        void print_out_int(int);
     private:
         // variables are constructed than costructor defines them
         LiquidCrystal _lcd;
         char _buffer[16];
-        int rs;
-        int rw;
-        int enable;
-        int d7;
-        int d6;
-        int d5;
-        int d4;
         
 };
 
