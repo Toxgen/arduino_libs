4c9cf06c17f13288dfe9cbba380802f1eb20eae5
= <=>
2025-10-18 00:33:03 -0400

    minor refactoring, moved into arduino ide

--- src/screen.cpp
+++ src/screen.cpp
@@ -52,203 +52,14 @@ void Screen::print_out_int(int input)
 [1] [2] [3] [4] [5] [6] [7] [hand left 8] [face 9] [hand right 10] 
 [1] [2] [3] [4] [5] [6] [7] [8] [lower body 9]
 */
-byte feet[8] = {
-  0b00100,
-  0b00100,
-  0b00100,
-  0b00100,
-  0b01010,
-  0b10001,
-  0b00000,
-  0b00000};
-
-byte head_0_to_2_and_7[8] = {
-  0b11111,
-  0b10001,
-  0b11011,
-  0b10001,
-  0b11111,
-  0b00100,
-  0b11111,
-  0b00000};
-
-byte head_3_to_4[8] = {
-  0b11111,
-  0b10001,
-  0b10101,
-  0b10001,
-  0b11111,
-  0b00100,
-  0b01110,
-  0b10101};
-
-byte head_5[8] = {
-  0b11111,
-  0b10001,
-  0b10101,
-  0b10001,
-  0b11111,
-  0b00100,
-  0b01110,
-  0b10111};
-
-byte head_6[8] = {
-  0b11111,
-  0b10001,
-  0b10101,
-  0b10001,
-  0b11111,
-  0b00100,
-  0b01110,
-  0b10111};
-
-// [0] both, [1] right
-btye hand_start[8] = {
-  0b00000,
-  0b00000,
-  0b00100,
-  0b10101,
-  0b01110,
-  0b00100,
-  0b11100,
-  0b00000};
-
-// [1] left, [2] right, **[6] left**
-byte hand_next0[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00100,
-  0b10101,
-  0b01110,
-  0b00111,
-  0b00000};
-
-// [2] left, [3] right
-byte hand_next1[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00101,
-  0b11110,
-  0b00011};
-
-// [3] left
-byte hand_next2_left[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b11111,
-  0b00011};
-
-// [4] left
-byte hand_next3_left[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00101,
-  0b11110,
-  0b00111};
-
-// [4] right
-byte hand_next3_right[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b10001,
-  0b01110,
-  0b10000};
-
-// [5] left
-byte hand_next4_left[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b10101,
-  0b01111,
-  0b00001};
-
-// [5] right
-byte hand_next4_right[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b01111,
-  0b11100};
-
-// [6] right
-byte hand_next5_right[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b10101,
-  0b01110,
-  0b11000};
-
-// [7] left
-byte hand_next6_left[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b10101,
-  0b01110,
-  0b00100,
-  0b00111,
-  0b00000};
-
-// [7] right
-byte hand_next6_right[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b10101,
-  0b01110,
-  0b11000,
-  0b00000};
-
 
 void Screen::print_out_six_seven(uint8_t frame) 
 {
-  lcd.createChar(0, feet);
-
-  lcd.createChar(1, head_0_to_2_and_7);
-  lcd.createChar(2, head_3_to_4);
-  lcd.createChar(3, head_5);
-  lcd.createChar(4, head_6);
-
-  lcd.createChar(5, hand_start);
-  lcd.createChar(6, hand_next0);
-  lcd.createChar(7, hand_next1);
-  lcd.createChar(8, hand_next2_left);
-  lcd.createChar(9, hand_next3_left);
-  lcd.createChar(10, hand_next3_right);
-  lcd.createChar(11, hand_next4_left);
-  lcd.createChar(12, hand_next4_right);
-  lcd.createChar(13, hand_next5_right);
-  lcd.createChar(14, hand_next6_left);
-  lcd.createChar(15, hand_next6_right);
 
   // (0, 0) is top left, (0 column, 0 row)
   switch (frame) {
     case 0:
-      lcd.setCursor(7, 0);
+      lcd.setCursor(6, 0);
       lcd.write(byte(5)); // both hands
       lcd.setCursor(8, 0);
       lcd.write(byte(1)); // face
@@ -258,7 +69,7 @@ void Screen::print_out_six_seven(uint8_t frame)
       lcd.write(byte(0)); // feet
       break;
     case 1:
-      lcd.setCursor(7, 0);
+      lcd.setCursor(6, 0);
       lcd.write(byte(6)); // left hand
       lcd.setCursor(8, 0);
       lcd.write(byte(2)); // face
@@ -268,7 +79,7 @@ void Screen::print_out_six_seven(uint8_t frame)
       lcd.write(byte(0)); // feet
       break;
     case 2:
-      lcd.setCursor(7, 0);
+      lcd.setCursor(6, 0);
       lcd.write(byte(6)); // left hand
       lcd.setCursor(8, 0);
       lcd.write(byte(3)); // face
@@ -278,7 +89,7 @@ void Screen::print_out_six_seven(uint8_t frame)
       lcd.write(byte(0)); // feet
       break;
     case 3:
-      lcd.setCursor(7, 0);
+      lcd.setCursor(6, 0);
       lcd.write(byte(9)); // left hand
       lcd.setCursor(8, 0);
       lcd.write(byte(4)); // face
@@ -288,7 +99,7 @@ void Screen::print_out_six_seven(uint8_t frame)
       lcd.write(byte(0)); // feet
       break;
     case 4:
-      lcd.setCursor(7, 0);
+      lcd.setCursor(6, 0);
       lcd.write(byte(11)); // left hand
       lcd.setCursor(8, 0);
       lcd.write(byte(5)); // face
@@ -298,7 +109,7 @@ void Screen::print_out_six_seven(uint8_t frame)
       lcd.write(byte(0)); // feet
       break;
     case 5:
-      lcd.setCursor(7, 0);
+      lcd.setCursor(6, 0);
       lcd.write(byte(14)); // left hand
       lcd.setCursor(8, 0);
       lcd.write(byte(4)); // face
@@ -308,7 +119,7 @@ void Screen::print_out_six_seven(uint8_t frame)
       lcd.write(byte(0)); // feet
       break;
     case 6:
-      lcd.setCursor(7, 0);
+      lcd.setCursor(6, 0);
       lcd.write(byte(15)); // left hand
       lcd.setCursor(8, 0);
       lcd.write(byte(2)); // face
@@ -318,7 +129,7 @@ void Screen::print_out_six_seven(uint8_t frame)
       lcd.write(byte(0)); // feet
       break;
     case 7:
-      lcd.setCursor(7, 0);
+      lcd.setCursor(6, 0);
       lcd.write(byte(5)); // both hands
       lcd.setCursor(8, 0);
       lcd.write(byte(1)); // face
@@ -327,7 +138,7 @@ void Screen::print_out_six_seven(uint8_t frame)
       lcd.setCursor(8, 1);
       lcd.write(byte(0)); // feet
       break;
-      
+
   }
 } 
 
--- src/screen.h
+++ src/screen.h
@@ -11,6 +11,11 @@ class Screen
         // VSS is GND, VCC is 5V, RW is GND, VO is 1k resisetor to GND, A is 1k resistor to 5V, K is GND
         Screen(int rs, int enable, int d4, int d5, int d6, int d7) : _lcd(rs, enable, d4, d5, d6, d7) 
         {  _lcd.begin(16, 2); }
+
+        struct six_seven {
+            uint8_t column
+            uint8_t row
+        }
         
         void print_out(char* input);
         void print_out_int(int input);
